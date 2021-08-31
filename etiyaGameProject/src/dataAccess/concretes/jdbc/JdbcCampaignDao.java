20eb78767f335ce1b2b92ae3eec005ed26662df5
giizemozgun <84465477+giizemozgun@users.noreply.github.com>
2021-08-31 20:09:26 +0300

    etiyaGameProject

---
+++ etiyaGameProject/src/dataAccess/concretes/jdbc/JdbcCampaignDao.java
@@ -0,0 +1,41 @@
+package dataAccess.concretes.jdbc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import dataAccess.abstracts.CampaignDao;
+import entities.concretes.Campaign;
+
+public class JdbcCampaignDao implements CampaignDao{
+
+	List<Campaign> campaigns;
+
+	public JdbcCampaignDao() {
+		super();
+		this.campaigns = new ArrayList<Campaign>();
+	}
+	@Override
+	public List<Campaign> getAll() {
+		
+		return this.campaigns;
+	}
+
+	@Override
+	public void add(Campaign entity) {
+		this.campaigns.add(entity);
+		
+	}
+
+	@Override
+	public void update(Campaign entity) {
+		
+		
+	}
+
+	@Override
+	public void delete(Campaign entity) {
+		
+		
+	}
+
+}
